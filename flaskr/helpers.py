78aebdc84da97b1fd1ed9920a128f1bad880bce3
Conrad Schloer <conrad.schloer@gmail.com>
2019-01-07 20:51:29 -0500

    Fix bug

--- flaskr/helpers.py
+++ flaskr/helpers.py
@@ -2,7 +2,7 @@ def transform_species_votes(species_votes):
     ''' Transform species_votes from a list of votes to a sorted list of lists of votes '''
     vote_tally = {}
     for vote in species_votes:
-        vote_dict = vote.to_dict() if vote is dict else vote
+        vote_dict = vote.to_dict() if type(vote) is not dict else vote
         species_id = vote_dict['species_id']
         if species_id not in vote_tally:
             vote_tally[species_id] = []
