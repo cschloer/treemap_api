34984e715270a5fe5a1ea9bb882f783172ffcc55
Conrad Schloer <conrad.schloer@gmail.com>
2019-02-20 00:00:55 +0100

    Switch back to redis

--- flaskr/auth0.py
+++ flaskr/auth0.py
@@ -8,16 +8,15 @@ import os
 import time
 import requests
 from .redis import redis
-from .shelf import get_shelf
 from .settings import Settings
 
-def get_auth0_access_token(shelf):
+def get_auth0_access_token():
     expires_at = 0
-    if 'auth0_access_token_expires_at' in shelf:
-        expires_at = shelf['expires_at']
+    if redis.exists('auth0_access_token_expires_at'):
+        expires_at = redis.get('expires_at')
     expired = expires_at - time.time() < 0
 
-    if expired or not 'auth0_access_token' in shelf:
+    if expired or not redis.exists('auth0_access_token'):
         res = requests.post(
             'https://treemap.auth0.com/oauth/token',
             data={
@@ -30,65 +29,65 @@ def get_auth0_access_token(shelf):
         )
         token = res.json()
         expires_at = token['expires_in'] + int(time.time()) - 10
-        shelf['auth0_access_token_expires_at'] = expires_at
-        shelf['auth0_access_token'] = token['access_token']
+        redis.set('auth0_access_token_expires_at', expires_at)
+        redis.set('auth0_access_token', token['access_token'])
 
-    access_token = shelf['auth0_access_token']
+
+    access_token = redis.get('auth0_access_token')
     return access_token
 
 def get_user_names(user_ids):
     ''' A function that takes in a list of user_ids and returns a dictionary connecting user_id to user_name '''
     user_names = {}
     unknown_user_ids = []
-    with get_shelf() as shelf:
-        for user_id in user_ids:
-            user_name = shelf.get(user_id)
-            if user_name:
-                user_names[user_id] = user_name
-            else:
-                unknown_user_ids.append(user_id)
+    for user_id in user_ids:
+        user_name = redis.get(user_id)
+        if user_name:
+            user_names[user_id] = user_name
+        else:
+            unknown_user_ids.append(user_id)
 
-        if len(unknown_user_ids):
-            access_token = get_auth0_access_token(shelf)
-            params = {
-                'q': ' OR '.join([f'user_id:{user_id}' for user_id in unknown_user_ids]),
-                'search_engine': 'v3',
-            }
-            res = requests.get(
-                f'https://treemap.auth0.com/api/v2/users',
-                headers={
-                    'authorization': f'Bearer {access_token}'
-                },
-                params=params,
+    if len(unknown_user_ids):
+        access_token = get_auth0_access_token()
+        params = {
+            'q': ' OR '.join([f'user_id:{user_id}' for user_id in unknown_user_ids]),
+            'search_engine': 'v3',
+        }
+        res = requests.get(
+            f'https://treemap.auth0.com/api/v2/users',
+            headers={
+                'authorization': f'Bearer {access_token}'
+            },
+            params=params,
 
-            )
-            if res.status_code == 200:
-                users = res.json()
-                # Iterate through all of the newfound users
-                for user in users:
-                    user_name = 'user'
-                    if 'given_name' in user:
-                        first_name = user['given_name']
-                        last_initial = f' {user["family_name"][0]}.' if user['family_name'] else ''
-                        user_name = f'{first_name}{last_initial}'
-                    elif 'nickname' in user:
-                        user_name = user['nickname']
-                    elif 'name' in user:
-                        user_name = user['name']
-                    # Add the username to the return object
-                    user_names[user['user_id']] = user_name
-                    # Add the username to the cache
-                    shelf[user['user_id']] = user_name
-            else:
-                print(f'ERROR when making request to auth0: {res.status_code}')
+        )
+        if res.status_code == 200:
+            users = res.json()
+            # Iterate through all of the newfound users
+            for user in users:
+                user_name = 'user'
+                if 'given_name' in user:
+                    first_name = user['given_name']
+                    last_initial = f' {user["family_name"][0]}.' if user['family_name'] else ''
+                    user_name = f'{first_name}{last_initial}'
+                elif 'nickname' in user:
+                    user_name = user['nickname']
+                elif 'name' in user:
+                    user_name = user['name']
+                # Add the username to the return object
+                user_names[user['user_id']] = user_name
+                # Add the username to the cache
+                redis.set(user['user_id'], user_name)
+        else:
+            print(f'ERROR when making request to auth0: {res.status_code}')
 
-        for user_id in unknown_user_ids:
-            # Find all user ids that were not found
-            if user_id not in user_names:
-                user_names[user_id] = 'Unknown user'
-                shelf[user_id] = 'Unknown user'
+    for user_id in unknown_user_ids:
+        # Find all user ids that were not found
+        if user_id not in user_names:
+            user_names[user_id] = 'Unknown user'
+            redis.set(user_id, 'Unknown user')
 
-        return user_names
+    return user_names
 
 def get_token_auth_header():
     """Obtains the Access Token from the Authorization Header
--- flaskr/redis.py
+++ flaskr/redis.py
@@ -3,5 +3,6 @@ from .settings import Settings
 
 REDIS_HOST = Settings.get('REDIS_HOST')
 REDIS_PORT = Settings.get('REDIS_PORT')
+REDIS_PASSWORD = Settings.get('REDIS_PASSWORD')
 
-redis = redis.StrictRedis(REDIS_HOST, REDIS_PORT, charset="utf-8", decode_responses=True)
+redis = redis.StrictRedis(REDIS_HOST, REDIS_PORT, password=REDIS_PASSWORD, charset="utf-8", decode_responses=True)
